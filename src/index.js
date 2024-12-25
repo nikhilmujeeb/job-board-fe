084171fcc2459360d492dff14f7cb7eedaadf215
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-12-26 02:11:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext'; // Adjust the path if necessary
+import { ThemeProvider } from './context/ThemeContext';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
